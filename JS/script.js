f666cbe5cde02a1ad8429e90a1ee63bb2690ce39
Jibex-Banks <mudathirajibolamudathir@gmail.com>
2025-02-05 16:42:43 +0100

    3rd Commit

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('.menuBar');
 const closeBar = document.querySelector('.xBar');
+const Wbtn = document.querySelector('.Wbtn');
 
 menu.onclick = function(){
 const sideBar = document.querySelector('.sidebar');
@@ -9,4 +10,14 @@ const sideBar = document.querySelector('.sidebar');
 closeBar.onclick = function () {
     const sideBar = document.querySelector('.sidebar');
     sideBar.style.display='none'; 
+}
+
+Wbtn.onclick = function () {
+    const Name = document.getElementById('name');
+    const Email = document.getElementById('email');
+    if (Name.value != "" && Email.value != "") {
+        alert("Thank you!🤩, You've joined the waitlist");        
+    } else {
+        alert("Please fill in the form");
+    }
 }
